13eefe4c372560529614d6c57bcfac3610cda4c6
aboutwallart <mae@aboutwallart.com>
2026-06-04 10:09:57 +0100

    Add files via upload

--- api/blogs.js
+++ api/blogs.js
@@ -142,6 +142,7 @@ export default async function handler(req, res) {
               locked: cols[2] === 'LOCKED',
               status: cols[3] || '',
               action: cols[4] || '',
+              winnerUrl: cols[5] || '',
               source: cols[9] || '',
               intent: cols[10] || ''
             });
